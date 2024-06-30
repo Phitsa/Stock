f0bca07586173752fa2cbcf590e14983d8720bfe
Phitsa <anthony.felipe.ff@gmail.com>
2024-06-30 01:24:23 -0300

    Code Organization

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
+
 $url = $_SERVER["REQUEST_URI"];
-if ($url === "/" ) {
-    require "views/home.php";
-}
-
-if ($url === "/products") {
-    require "views/products.php";
-}
-
-if ($url === "/settings") {
-    require "views/settings.php";
-}
+$links = 
+[
+    '/' => 'views/home.php',
+    '/products' => 'views/products.php',
+    '/settings' => 'views/settings.php',
+    '/add-item' => 'views/add-item.php',
+];
+// lembrar de usar parse_url
+require $links[$url];
